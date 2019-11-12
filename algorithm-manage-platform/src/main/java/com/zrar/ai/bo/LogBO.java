c934150901e6bc746c64dbd76bea4923468f0e0b
hellozjf <908686171@qq.com>
2019-11-12 14:26:59 +0800

    版本1.1.0，增加描述

--- algorithm-manage-platform/src/main/java/com/zrar/ai/bo/LogBO.java
+++ algorithm-manage-platform/src/main/java/com/zrar/ai/bo/LogBO.java
@@ -15,10 +15,33 @@ import javax.persistence.Table;
 @Table(name = "log")
 public class LogBO extends BaseBO {
 
+    /**
+     * 日志文本
+     */
     private String content;
+
+    /**
+     * 日志描述
+     */
     private String description;
+
+    /**
+     * 调用耗时
+     */
     private Long cost;
+
+    /**
+     * 调用controller方法名称
+     */
     private String method;
+
+    /**
+     * 调用controller方法参数
+     */
     private String param;
+
+    /**
+     * 调用者IP
+     */
     private String ip;
 }
